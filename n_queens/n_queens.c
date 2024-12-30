089f81a3747bddbfb03f0bd795d74c16cfcbfd46
Moat423 <schluesselfehlt@gmail.com>
2024-12-30 12:39:48 +0100

    working as intended

--- n_queens/n_queens.c
+++ n_queens/n_queens.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// set a sequence to whatever n is
 void	ft_bn(void *s, size_t i, int n)
 {
 	unsigned char *p = s;
@@ -23,75 +24,63 @@ void	ft_bn(void *s, size_t i, int n)
 		*p++ = n;
 }
 
-int	is_safe(int board_size, int *solutions, int curr)
+// check if queen can be placed in a given position
+int	is_safe(int board_size, int *solutions, int col)
 {
 	int	 i = -1;
 
-	while (++i >= 0)
+	while (++i < col)
 	{
-		if (solutions[curr] == solutions[i])
+		if (solutions[col] == solutions[i])
 			return (0);
-		if (solutions[curr] == solutions[i] - i + curr)
+		if (solutions[col] == solutions[i] - i + col)
 			return (0);
-		if (solutions[curr] == solutions[i] + i - curr)
+		if (solutions[col] == solutions[i] + i - col)
 			return (0);
 	}
 	return (1);
 }
 
+// print state of board (columns seperated by spaces, position of queen as number from 0 to n-1)
 void	print_board(int solutions[], int board_size)
 {
 	int	i = -1;
 
 	(void) board_size;
-
 	while (++i < board_size)
 		printf("%d ", solutions[i]);
 	printf("\n");
 }
 
+// backtrack to find solution
 void	calc_board(int board_size, int col, int *solutions)
 {
-	// if (is_safe(board_size, solutions, col))
-	// {
-	// 	if (col == board_size - 1)
-	// 		print_board(solutions, board_size);
-	// 	else
-	// 		calc_board(board_size, col + 1, solutions);
-	// }
+	int i = -1;
 
-	while (col < board_size)
+	if (col == board_size)
 	{
-		solutions[col]++;
-		if (solutions[col] == board_size)
-		{
-			solutions[col] = 0;
-			col--;
-		}
-		else if (is_safe(board_size, solutions, col))
-		{
-			if (col == board_size - 1)
-				print_board(solutions, board_size);
-			else
-				calc_board(board_size, col + 1, solutions);
-		}
+		print_board(solutions, board_size);
+		return ;
+	}
+	while (++i < board_size)
+	{
+		solutions[col] = i;
+		if (is_safe(board_size, solutions, col))
+			calc_board(board_size, col + 1, solutions);
+		solutions[col] = -1;
 	}
 }
-
 
 int	main(int argc, char *argv[])
 {
 	int	board_size;
-
 	if (argc != 2)
 		return (1);
 	board_size = atoi(argv[1]);
 	if (board_size < 1)
 		return (1);
 	int solutions[board_size];
-	ft_bn(solutions, board_size, -1);
+	ft_bn(solutions, board_size, -1); // set board to -1 in all slots
 	calc_board(board_size, 0, solutions);
-	print_board(solutions, board_size);
-
+	return (0);
 }
-
